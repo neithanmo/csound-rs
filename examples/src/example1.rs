a0b857d78893897442c44586e38c66306a09a14f
TheDarkSword <TheDarkSword01@gmail.com>
2025-05-22 17:17:35 +0200

    Removed useless logging prompt in example1

--- examples/src/example1.rs
+++ examples/src/example1.rs
@@ -5,14 +5,11 @@ fn main() {
 
     // Open the system audio driver
     cs.set_option("-odac").unwrap();
-    cs.set_option("-d").unwrap();
 
     let args = ["csound", "examples/test1.csd"];
     cs.compile(&args).unwrap();
-    
+
     cs.start().unwrap();
-    
-    while !cs.perform_ksmps() {
-        println!("Performing ksmps...");
-    }
+
+    while !cs.perform_ksmps() {}
 }
